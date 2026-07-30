8efcf113b29b1d41904bcef07f2244c628decee5
Matthew Yacavone <matthew@yacavone.net>
2026-07-30 18:47:46 -0400

    add back rollupOptions to vite config and get it to typecheck, fix thin spaces in default text

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   base: "/ttTikkun/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(import.meta.dirname, "index.html"),
+        transliterate: resolve(import.meta.dirname, "transliterate.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       // hack until I actually export transliteration.js
       "havarotjs/transliteration": resolve(
-        __dirname,
+        import.meta.dirname,
         "node_modules/havarotjs/dist/esm/transliteration.js"
       ),
     },
